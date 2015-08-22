8b2fac5794e99cdf34ab7c4b49c797946e67af24
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-08-22 06:57:22 +0200

    Project started

---
+++ EmergenceDesert/Assets/Scripts/CameraNavigation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraNavigation : MonoBehaviour {
+
+    [SerializeField]
+    float walkSpeed = 5;
+    [SerializeField]
+    float runSpeed = 10;
+
+    float v, h;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            transform.Translate(h * runSpeed * Time.deltaTime, 0, v * runSpeed * Time.deltaTime);
+        else
+            transform.Translate(h * walkSpeed * Time.deltaTime, 0, v * walkSpeed * Time.deltaTime);
+    }
+}
