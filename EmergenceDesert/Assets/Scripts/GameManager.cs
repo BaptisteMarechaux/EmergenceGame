4e5c2cf31fd0dd31cf7d82fa0de651bff5abaf39
PrLayton <gogokoko3@hotmail.fr>
2015-08-22 13:11:26 +0200

    Figth v0.5

--- EmergenceDesert/Assets/Scripts/GameManager.cs
+++ EmergenceDesert/Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour {
 	Vector3 destination;
 	float t;
 	float childTime;
+	float fightTime;
 	bool seeWater;
 	bool fight;
 	bool canMakeChild;
@@ -15,7 +16,9 @@ public class GameManager : MonoBehaviour {
 	bool makeChild;
 	List<GameObject> friends = new List<GameObject>();
 	List<GameObject> listRemove = new List<GameObject> ();
+	public Material defaultMaterial;
 	public Material fightMaterial;
+	public GameObject model;
 
 	// Use this for initialization
 	void Start () {
@@ -28,11 +31,12 @@ public class GameManager : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		// Supprime ceux qui sont trop loin
 		if(listRemove.Count != 0)
 			listRemove.Clear ();
 		foreach (GameObject friend in friends) {
-			if(Vector3.Distance(cube.transform.position, friend.transform.position) > 2){
+			if(Vector3.Distance(cube.transform.position, friend.transform.position) > 3){
 				listRemove.Add(friend);
 			}
 		}
@@ -40,30 +44,47 @@ public class GameManager : MonoBehaviour {
 			if(friends.Contains(friend))
 				friends.Remove(friend);
 		}
+		// Enfants
 		childTime += 1f*Time.deltaTime;
 		if (makeChild == true && Random.Range(0.0f, 1.0f)> 0.5f && childTime > 1f && canMakeChild) {
-			GameObject.Instantiate(this);
-			childTime = 0f;
-			canMakeChild = false;
+			foreach (GameObject friend in friends) {
+				if(Vector3.Distance(cube.transform.position, friend.transform.position) < 2){
+					GameObject.Instantiate(this);
+					childTime = 0f;
+					canMakeChild = false;
+				}
+			}
 		}
+		// Changement de destination
 		t += Random.Range(0.5f, 3f)*Time.deltaTime;
 		if (t > 2) {
 			destination = new Vector3 (cube.transform.position.x + Random.Range (-4.0f, 4.0f)
 		                           , 1f, cube.transform.position.z + Random.Range (-4.0f, 4.0f));
 			t=0.0f;
 		}
+		// Eau
 		if (seeWater && Water != null) {
 			if(Vector3.Distance(this.transform.position, Water.transform.position) > 5){
 				destination = Water.transform.position;
 			}
 		}
+		// Combat
+		fightTime += 1f*Time.deltaTime;
 		if (fight && friends.Count != 0) {
-			int index = Random.Range(0,friends.Count-1);
-			Debug.Log(index.ToString() + friends.Count.ToString());
-			if(index < friends.Count){
-				destination = friends[index].transform.position;
+			int index = Random.Range (0, friends.Count - 1);
+			//Debug.Log (index.ToString () + friends.Count.ToString ());
+			if (index < friends.Count) {
+				destination = friends [index].transform.position;
 			}
-		}
+			model.GetComponent<Renderer> ().material = fightMaterial;
+			if(Random.Range(0.0f, 1.0f)> 0.25f && fightTime > 2f){
+				model.tag = "Dead";
+				Destroy(this);
+				fightTime = 0;
+			}
+		} else
+			model.GetComponent<Renderer> ().material = defaultMaterial;
+		// Déplacements
 		cube.transform.rotation = Quaternion.Lerp (cube.transform.rotation, Quaternion.identity, 1.5f * Time.deltaTime);
 		cube.transform.position = Vector3.Lerp(cube.transform.position
 		        , destination, 1f*Time.deltaTime);
@@ -74,7 +95,6 @@ public class GameManager : MonoBehaviour {
 			seeWater = true;
 			Water = collider.gameObject;
 		}
-
 	}
 
 	void OnCollisionEnter(Collision collision){
@@ -82,11 +102,11 @@ public class GameManager : MonoBehaviour {
 			friends.Add(collision.gameObject);
 
 		}
-		if (friends.Count >= 5) {
+		if (friends.Count >= 4) {
+			//Debug.Log(friends.Count.ToString());
 			fight = true;
 		}
 		if (friends.Count == 2) {
-
 			makeChild = true;
 		}else{
 			makeChild = false;
@@ -98,14 +118,13 @@ public class GameManager : MonoBehaviour {
 			seeWater = false;
 			Water = null;
 		}
-
 	}
 
 	void OnCollisionExit(Collision collision){
 		if (collision.collider.tag == "Character") {
 			friends.Remove(collision.gameObject);
 		}
-		if (friends.Count < 5) {
+		if (friends.Count < 4) {
 			fight = false;
 		}
 		if (friends.Count != 2) {
