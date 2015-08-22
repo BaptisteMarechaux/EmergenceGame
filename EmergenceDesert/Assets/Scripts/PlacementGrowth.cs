89bb406f2323bcdb57caa9954f476d047bff7204
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-08-22 14:54:58 +0200

    Water behavior added

---
+++ EmergenceDesert/Assets/Scripts/PlacementGrowth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacementGrowth : MonoBehaviour {
+    [SerializeField]
+    Vector3 minimummScale;
+
+    [SerializeField]
+    Vector3 maximumScale;
+
+    void OnEnable()
+    {
+        transform.localScale = minimummScale;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if(transform.localScale.x >= maximumScale.x*0.96f)
+        {
+            
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, maximumScale, Time.deltaTime);
+        }
+        
+	}
+}
--- EmergenceDesert/Assets/Scripts/SunManager.cs
+++ EmergenceDesert/Assets/Scripts/SunManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SunManager : MonoBehaviour {
 
@@ -7,12 +8,32 @@ public class SunManager : MonoBehaviour {
     ParticleSystem rainParticle;
 
     [SerializeField]
+    Transform rainParticleTransform;
+
+    [SerializeField]
     Transform SelectorPosition;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    [SerializeField]
+    GameObject[] waterSources;
+
+    [SerializeField]
+    int disponibleWaterSources;
+
+    [SerializeField]
+    Text waterSourcesDispoText;
+
+    bool startedPlacingWater;
+    Vector3 selectedPosition;
+    int selectedWaterSourceToPlace;
+
+    float t;
+    int layerMask = 1 << 9;
+
+    // Use this for initialization
+    void Start () {
+        waterSourcesDispoText.text = "Sources Dispo : " + disponibleWaterSources;
+        t = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,7 +42,45 @@ public class SunManager : MonoBehaviour {
             //Make it rain
             rainParticle.Play();
         }
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(!startedPlacingWater)
+            {
+                if (disponibleWaterSources > 0)
+                {
+                    for (int i = 0; i < waterSources.Length; i++)
+                    {
+                        if (!waterSources[i].activeSelf)
+                        {
+                            startedPlacingWater = true;
+                            selectedPosition = SelectorPosition.position;
+                            selectedWaterSourceToPlace = i;
+                            rainParticle.gameObject.transform.position.Set(selectedPosition.x, rainParticle.transform.position.y, selectedPosition.z);
+                            rainParticle.Play();
+                            break;
+                        }
+                    }
+                    disponibleWaterSources--;
+                    waterSourcesDispoText.text = "Sources Dispo : " + disponibleWaterSources.ToString();
+                }
+                else
+                {
+
+                }
+            }
             
+            
+        }
+
+        if (startedPlacingWater)
+        {
+            if(PlacingWater())
+            {
+                waterSources[selectedWaterSourceToPlace].SetActive(true);
+                waterSources[selectedWaterSourceToPlace].transform.position = selectedPosition;
+            }
+        }
 
     }
 
@@ -29,11 +88,26 @@ public class SunManager : MonoBehaviour {
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 500))
+        if (Physics.Raycast(ray, out hit, 500, layerMask))
         {
             Debug.DrawLine(ray.origin, hit.point);
             //if (hit.transform == truc) ;
             SelectorPosition.position = new Vector3(hit.point.x, SelectorPosition.position.y, hit.point.z);
+            Debug.Log("Did Hit");
         }
+    }
+
+    bool PlacingWater()
+    {
+        t += Time.deltaTime;
+        if(t>=rainParticle.duration*0.5f)
+        {
+            rainParticle.Stop();
+            startedPlacingWater = false;
+            t = 0;
+            return true;
+        }
+
+        return false;
     }
 }
