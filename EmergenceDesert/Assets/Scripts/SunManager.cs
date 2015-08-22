8cb1b43f175da0d07b1b46f0b5297318550db426
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-08-22 13:12:04 +0200

    SelectorForWater

---
+++ EmergenceDesert/Assets/Scripts/SunManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunManager : MonoBehaviour {
+
+    [SerializeField]
+    ParticleSystem rainParticle;
+
+    [SerializeField]
+    Transform SelectorPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.R))
+        {
+            //Make it rain
+            rainParticle.Play();
+        }
+            
+
+    }
+
+    void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 500))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            //if (hit.transform == truc) ;
+            SelectorPosition.position = new Vector3(hit.point.x, SelectorPosition.position.y, hit.point.z);
+        }
+    }
+}
