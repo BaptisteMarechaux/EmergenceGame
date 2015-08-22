e8add6e087b07f9f1608cf8053399b0ed28bbb09
PrLayton <gogokoko3@hotmail.fr>
2015-08-22 11:12:01 +0200

    Fight

--- EmergenceDesert/Assets/Scripts/GameManager.cs
+++ EmergenceDesert/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
@@ -7,7 +8,9 @@ public class GameManager : MonoBehaviour {
 	Vector3 destination;
 	float t;
 	bool seeWater;
+	bool fight;
 	GameObject Water;
+	List<GameObject> friends = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +18,7 @@ public class GameManager : MonoBehaviour {
 		destination = cube.transform.position;
 		seeWater = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		t += Random.Range(0.5f, 3f)*Time.deltaTime;
@@ -24,9 +27,17 @@ public class GameManager : MonoBehaviour {
 		                           , 1f, cube.transform.position.z + Random.Range (-4.0f, 4.0f));
 			t=0.0f;
 		}
-		if (Water) {
+		if (seeWater && Water != null) {
 			if(Vector3.Distance(this.transform.position, Water.transform.position) > 5){
 				destination = Water.transform.position;
+			}
+		}
+		Debug.Log (fight + friends.Count.ToString());
+		if (fight && friends.Count != 0) {
+			int index = Random.Range(0,friends.Count-1);
+			Debug.Log(index.ToString() + friends.Count.ToString());
+			if(index < friends.Count){
+				destination = friends[index].transform.position;
 			}
 		}
 		cube.transform.position = Vector3.Lerp(cube.transform.position
@@ -34,10 +45,34 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider collider){
-		Debug.Log (this.name);
 		if(collider.name == "Water"){
 			seeWater = true;
 			Water = collider.gameObject;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.collider.tag == "Character") {
+			friends.Add(collision.gameObject);
+		}
+		if (friends.Count >= 5) {
+			fight = true;
+		}
+	}
+
+	void OnTriggerExit(Collider collider){
+		if(collider.name == "Water"){
+			seeWater = false;
+			Water = null;
+		}
+	}
+
+	void OnCollisionExit(Collision collision){
+		if (collision.collider.tag == "Character") {
+			friends.Remove(collision.gameObject);
+		}
+		if (friends.Count < 5) {
+			fight = false;
 		}
 	}
 }
