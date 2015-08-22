b5d15cac51a3873992af642948b84a090e6611dd
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-08-22 09:15:06 +0200

    WaterPoint

---
+++ EmergenceDesert/Assets/Scripts/WaterPoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterPoint : MonoBehaviour {
+    [SerializeField]
+    int countryNumber;
+    public int CountryNumber{
+        get { return countryNumber; }
+        set {countryNumber = value; }
+    }
+
+    [SerializeField]
+    Color waterColor;
+    [SerializeField]
+    Renderer waterRenderer;
+
+    public bool activated = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
