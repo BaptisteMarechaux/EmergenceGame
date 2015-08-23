77a4db4bd3ab5bae82832701322009a0152f4e8f
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2015-08-23 16:59:24 +0200

    ExitScreen

---
+++ EmergenceDesert/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitScript : MonoBehaviour {
+    [SerializeField]
+    SunManager sunManager;
+
+
+
+    void OnEnable()
+    {
+        sunManager.enabled = false;
+    }
+
+    void OnDisable()
+    {
+        sunManager.enabled = true;
+    }
+
+    public void ExitGame()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void NotExitGame()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
